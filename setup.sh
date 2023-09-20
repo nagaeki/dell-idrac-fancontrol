3ba4aca0316733403b2ecc8109981defd37aa382
Nagaeki Tomoki <i@eki.moe>
2023-09-20 22:14:24 +0900

    fix typo leading to error

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ ipmitool raw 0x30 0x30 0x01 0x00
 
 wget https://github.com/nagaeki/dell-idrac-fancontrol/raw/main/fancontrol.sh -O /etc/fancontrol.sh
 chmod +x /etc/fancontrol.sh
-wget https://github.com/nagaeki/dell-idrac-fancontrol/raw/main/fancontrol.service -O /etc/systemd/system/fancontrol.sh
+wget https://github.com/nagaeki/dell-idrac-fancontrol/raw/main/fancontrol.service -O /etc/systemd/system/fancontrol.service
 systemctl daemon-reload
 systemctl enable fancontrol --now
